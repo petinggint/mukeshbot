3a9d2e4633b93f13bbdf6ff86f53e61ca3f5a72b
petinggint <155607304+petinggint@users.noreply.github.com>
2024-07-10 11:40:42 +0700

    F config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -3,7 +3,7 @@ class Config(object):
     API_ID =None 
     API_HASH = ""
     TOKEN = ""  
-    OWNER_ID=None
+    OWNER_ID=7353842230
     
     SUPPORT_CHAT = "" 
     START_IMG = ""
@@ -17,12 +17,12 @@ class Config(object):
     TIME_API_KEY = ""
 
     
-    BL_CHATS = [] 
-    DRAGONS = []
-    DEV_USERS = []  
-    DEMONS = [] 
-    TIGERS = []  
-    WOLVES = [] 
+    BL_CHATS = [7353842230] 
+    DRAGONS = [7353842230]
+    DEV_USERS = [7353842230]  
+    DEMONS = [7353842230] 
+    TIGERS = [7353842230]  
+    WOLVES = [7353842230] 
 
     ALLOW_CHATS = True
     ALLOW_EXCL = True
